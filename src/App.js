a7da5e7efc281023718099f411fd8a46bd7c16ad
rokaya magdi <rokaiamagdi@gmail.com>
2021-02-24 13:13:11 +0200

    use container class

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 const App = () => {
-  return <div className="container">Hello from react App</div>;
+  return (
+    <div className="container mt-4">
+      <h1>Hello from react App</h1>
+    </div>
+  );
 };
 
 export default App;
